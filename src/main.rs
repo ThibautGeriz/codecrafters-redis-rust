c7767c49bec34b7cb179ffec876202e52352d995
Thibaut Gery <thibaut.gery@datadoghq.com>
2023-02-20 20:53:37 +0100

    handle multiple pong

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
-use std::net::TcpListener;
 use std::io::prelude::Write;
+use std::io::{BufRead, BufReader, BufWriter};
+use std::net::TcpListener;
 
-fn main() {
+const PONG: &[u8] = "+PONG\r\n".as_bytes();
+
+fn main() -> std::result::Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
+    loop {
+        let (stream, addr) = listener.accept()?;
+        eprintln!("accepted: {addr:?}");
+        handle_one_stream(stream);
+    }
+}
 
-                _stream.write("+PONG\r\n".as_bytes()).unwrap();
+fn handle_one_stream(stream: std::net::TcpStream) {
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
+    loop {
+        match reader.fill_buf() {
+            Ok(buf) => {
+                if buf.is_empty() {
+                    return;
+                }
+                if let Err(e) = writer.write_all(PONG) {
+                    eprintln!("failed to write_all: {e:?}");
+                    return;
+                }
+                if let Err(e) = writer.flush() {
+                    eprint!("failed to flush: {e:?}");
+                    return;
+                }
+                let len = buf.len();
+                reader.consume(len);
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("failed to fill_buf: {e:?}");
+                return;
             }
         }
     }
