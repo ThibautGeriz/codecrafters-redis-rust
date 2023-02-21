a9849c0b3a5c910a897241f045ddb6ce56210b7d
Thibaut Gery <thibaut.gery@datadoghq.com>
2023-02-21 23:09:44 +0100

    Add support for multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,33 @@
-use std::io::prelude::Write;
-use std::io::{BufRead, BufReader, BufWriter};
-use std::net::TcpListener;
+use tokio::io::{AsyncWriteExt, Error, AsyncReadExt};
+use tokio::net::{TcpListener, TcpStream};
 
 const PONG: &[u8] = "+PONG\r\n".as_bytes();
 
-fn main() -> std::result::Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> std::result::Result<(), Error> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
-        let (stream, addr) = listener.accept()?;
+        let(mut stream, addr) = listener.accept().await?;
         eprintln!("accepted: {addr:?}");
-        handle_one_stream(stream);
+        tokio::spawn(async move {
+            handle_connection(&mut stream).await.unwrap();
+        });
     }
 }
 
-fn handle_one_stream(stream: std::net::TcpStream) {
-    let mut reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
+async fn handle_connection(stream: &mut TcpStream) -> std::result::Result<(), Error> {
+    let mut buf = [0; 512];
 
     loop {
-        match reader.fill_buf() {
-            Ok(buf) => {
-                if buf.is_empty() {
-                    return;
-                }
-                if let Err(e) = writer.write_all(PONG) {
-                    eprintln!("failed to write_all: {e:?}");
-                    return;
-                }
-                if let Err(e) = writer.flush() {
-                    eprint!("failed to flush: {e:?}");
-                    return;
-                }
-                let len = buf.len();
-                reader.consume(len);
-            }
-            Err(e) => {
-                eprintln!("failed to fill_buf: {e:?}");
-                return;
-            }
+        let bytes_read = stream.read(&mut buf).await?;
+        if bytes_read == 0 {
+            println!("client closed the connection");
+            break;
         }
+
+        stream.write(PONG).await?;
     }
+
+    Ok(())
 }
+
